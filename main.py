3d9ab6ea64a1a654bd83693f36b4b1632248596c
Lugas <maxim.lugovsky@gmail.com>
2023-01-06 16:34:26 +0300

    SEA BATTLE for two players

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ step_x = size_canvas_x // s_x  # horizontal step
 step_y = size_canvas_y // s_y  # vertical step
 size_canvas_x = step_x * s_x
 size_canvas_y = step_y * s_y
-menu_x = step_x * 4
+delta_menu_x = 4
+menu_x = step_x * delta_menu_x
 menu_y = 40
 ships = s_x // 2  # maximum number of ships
 ship_len_1 = s_x // 5  # length of the first type of ship
@@ -30,13 +31,13 @@ ships_list = []  # ships list player1 and player2
 
 def on_closing():
     global app_running
-    if messagebox.askokcancel('Exit', 'Are u sure?'):
+    if messagebox.askyesno('Exit', 'Are u sure?'):
         app_running = False
         tk.destroy()
 
 
 tk.protocol('WM_DELETE_WINDOW', on_closing)
-tk.title('BATTLE SEA')
+tk.title('SEA BATTLE')
 tk.resizable(0, 0)
 tk.wm_attributes('-topmost', 1)
 canvas = Canvas(tk, width=size_canvas_x + menu_x + size_canvas_x, height=size_canvas_y + menu_y, bd=0,
@@ -108,10 +109,10 @@ def button_begin_again():
     boom = [[0 for i in range(s_y)] for i in range(s_x)]
 
 
-b0 = Button(tk, text='Show player№1 ships', command=button_show_enemy1)
+b0 = Button(tk, text='Show player №1 ships', command=button_show_enemy1)
 b0.place(x=size_canvas_x + 20, y=30)
 
-b1 = Button(tk, text='Show player№2 ships', command=button_show_enemy2)
+b1 = Button(tk, text='Show player №2 ships', command=button_show_enemy2)
 b1.place(x=size_canvas_x + 20, y=70)
 
 b2 = Button(tk, text='Restart game', command=button_begin_again)
@@ -126,12 +127,36 @@ def draw_point(x, y):
                                  y * step_y + step_y - step_y // 3, fill="white")
         list_ids.append(id1)
         list_ids.append(id2)
+
     if enemy_ships1[y][x] > 0:
         color = "black"
         id1 = canvas.create_rectangle(x * step_x, y * step_y + step_y // 2 - step_y // 10, x * step_x + step_x,
                                       y * step_y + step_y // 2 + step_y // 10, fill=color)
         id2 = canvas.create_rectangle(x * step_x + step_x // 2 - step_x // 10, y * step_y,
                                       x * step_x + step_x // 2 + step_x // 10, y * step_y + step_y, fill=color)
+        list_ids.append(id1)
+        list_ids.append(id2)
+
+
+def draw_point2(x, y, offset_x=size_canvas_x + menu_x):
+    if enemy_ships2[y][x] == 0:
+        color = "red"
+        id1 = canvas.create_oval(offset_x + x * step_x, y * step_y, offset_x + x * step_x + step_x, y * step_y + step_y,
+                                 fill=color)
+        id2 = canvas.create_oval(offset_x + x * step_x + step_x // 3, y * step_y + step_y // 3,
+                                 offset_x + x * step_x + step_x - step_x // 3,
+                                 y * step_y + step_y - step_y // 3, fill="white")
+        list_ids.append(id1)
+        list_ids.append(id2)
+
+    if enemy_ships2[y][x] > 0:
+        color = "black"
+        id1 = canvas.create_rectangle(offset_x + x * step_x, y * step_y + step_y // 2 - step_y // 10,
+                                      offset_x + x * step_x + step_x,
+                                      y * step_y + step_y // 2 + step_y // 10, fill=color)
+        id2 = canvas.create_rectangle(offset_x + x * step_x + step_x // 2 - step_x // 10, y * step_y,
+                                      offset_x + x * step_x + step_x // 2 + step_x // 10, y * step_y + step_y,
+                                      fill=color)
         list_ids.append(id1)
         list_ids.append(id2)
 
@@ -157,8 +182,18 @@ def check_winner2():
     return win
 
 
+def check_winner2_player2():
+    win = True
+    for i in range(0, s_x):
+        for j in range(0, s_y):
+            if enemy_ships2[j][i] > 0:
+                if points2[j][i] == -1:
+                    win = False
+    return win
+
+
 def add_to_all(event):
-    global points1
+    global points1, points2
     _type = 0  # left mouse button
     if event.num == 3:
         _type = 1  # right mouse button
@@ -166,14 +201,26 @@ def add_to_all(event):
     mouse_y = canvas.winfo_pointery() - canvas.winfo_rooty()
     ip_x = mouse_x // step_x
     ip_y = mouse_y // step_y
+
+    # first game field
     if ip_x < s_x and ip_y < s_y:
         if points1[ip_y][ip_x] == -1:
             points1[ip_y][ip_x] = _type
             draw_point(ip_x, ip_y)
-            if check_winner(ip_x, ip_y):
-                print("You're winner!!")
+            if check_winner2():
+                messagebox.showinfo('SEA BATTLE', "Second player win!!!!")
                 points1 = [[10 for i in range(s_y)] for i in range(s_x)]
-        print(len(list_ids))
+                points2 = [[10 for i in range(s_y)] for i in range(s_x)]
+
+    # second game field
+    if ip_x >= s_x + delta_menu_x and ip_x <= s_x + s_x + delta_menu_x and ip_y < s_y:
+        if points2[ip_y][ip_x - s_x - delta_menu_x] == -1:
+            points2[ip_y][ip_x - s_x - delta_menu_x] = _type
+            draw_point2(ip_x - s_x - delta_menu_x, ip_y)
+            if check_winner2_player2():
+                messagebox.showinfo('SEA BATTLE', "First player win!!!!")
+                points1 = [[10 for i in range(s_y)] for i in range(s_x)]
+                points2 = [[10 for i in range(s_y)] for i in range(s_x)]
 
 
 canvas.bind_all('<Button-1>', add_to_all)  # left mouse button
@@ -185,6 +232,7 @@ def generate_ships_list():
     ships_list = []
     for i in range(0, ships):  # generating a list of random ship lengths
         ships_list.append(random.choice([ship_len_1, ship_len_2, ship_len_3]))
+
 
 def generate_enemy_ships():
     global ships_list
@@ -252,6 +300,7 @@ def generate_enemy_ships():
                     sum_1_enemy = sum_1_enemy + 1
 
     return enemy_ships
+
 
 generate_ships_list()
 enemy_ships1 = generate_enemy_ships()
